6bfa86b9b37ca43dd1460dbf421ed13310014d8c
squeek <squeek502@hotmail.com>
2014-04-20 18:26:13 -0700

    Use the first HarvestCraft crop's texture in its iconArray (asparagus) as the crops block's blockIcon

--- java/squeek/harvestcraftwaila/fixers/IconFixer.java
+++ java/squeek/harvestcraftwaila/fixers/IconFixer.java
@@ -5,6 +5,7 @@ import net.minecraft.util.Icon;
 import net.minecraftforge.client.event.TextureStitchEvent;
 import net.minecraftforge.event.EventPriority;
 import net.minecraftforge.event.ForgeSubscribe;
+import assets.pamharvestcraft.BlockPamCrop;
 import assets.pamharvestcraft.PamHarvestCraft;
 import cpw.mods.fml.common.ObfuscationReflectionHelper;
 import cpw.mods.fml.relauncher.ReflectionHelper;
@@ -16,7 +17,7 @@ public class IconFixer
 	{
 		try
 		{
-			Icon icon = Block.crops.getIcon(0, 0);
+			Icon icon = ((BlockPamCrop) PamHarvestCraft.pamCrop).iconArray[0][0];
 			ReflectionHelper.setPrivateValue(Block.class, PamHarvestCraft.pamCrop, icon, ObfuscationReflectionHelper.remapFieldNames(Block.class.getName(), "blockIcon", "field_94336_cN", "cW"));
 		}
 		catch (Exception e)
